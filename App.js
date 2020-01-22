19b9255949f63a8ff0efd79952bfe7b2e92bf076
Siva Krishna <shivataraya@gmail.com>
2020-01-21 20:04:21 -0600

    add show screen to list single blog

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import IndexScreen from "./src/screens/IndexScreen";
+import ShowScreen from "./src/screens/ShowScreen";
 import { Provider } from "./src/context/BlogContext";
 
 const navigator = createStackNavigator(
   {
-    Index: IndexScreen
+    Index: IndexScreen,
+    Show: ShowScreen
   },
   {
     initialRouteName: "Index",
