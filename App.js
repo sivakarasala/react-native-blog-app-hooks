344ad212b759cd7150e5b891760ad9b4b9ea85dc
Siva Krishna <shivataraya@gmail.com>
2020-01-21 22:30:50 -0600

    add create screen

--- App.js
+++ App.js
@@ -3,12 +3,15 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import IndexScreen from "./src/screens/IndexScreen";
 import ShowScreen from "./src/screens/ShowScreen";
+import CreateScreen from "./src/screens/CreateScreen";
+
 import { Provider } from "./src/context/BlogContext";
 
 const navigator = createStackNavigator(
   {
     Index: IndexScreen,
-    Show: ShowScreen
+    Show: ShowScreen,
+    Create: CreateScreen
   },
   {
     initialRouteName: "Index",
---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const CreateScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Hara Hara</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CreateScreen;
