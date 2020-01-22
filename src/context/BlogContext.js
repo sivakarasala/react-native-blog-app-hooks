b24e2677dd565929f98baa1fbcd6fea87a458e6a
Siva Krishna <shivataraya@gmail.com>
2020-01-21 19:23:08 -0600

    add data to context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider>{children}</BlogContext.Provider>;
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  const addBlogPost = () => {
+    setBlogPosts([
+      ...blogPosts,
+      { title: `Blog Post #${blogPosts.length + 1}` }
+    ]);
+  };
+
+  return (
+    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+      {children}
+    </BlogContext.Provider>
+  );
 };
+
+export default BlogContext;
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,18 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
+  const { data, addBlogPost } = useContext(BlogContext);
   return (
     <View>
       <Text>Aum Namah Shivaya</Text>
+      <Button title="Add Blog Post" onPress={addBlogPost} />
+      <FlatList
+        data={data}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({ item }) => <Text>{item.title}</Text>}
+      />
     </View>
   );
 };
