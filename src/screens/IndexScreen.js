69c1e6013a830f658b2c77a5b3ee8b37647fc8da
Siva Krishna <shivataraya@gmail.com>
2020-01-21 19:45:43 -0600

    refactor to reusable context

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,15 +1,15 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
-import BlogContext from "../context/BlogContext";
+import { Context } from "../context/BlogContext";
 
 const IndexScreen = () => {
-  const { data, addBlogPost } = useContext(BlogContext);
+  const { state, addBlogPost } = useContext(Context);
   return (
     <View>
       <Text>Aum Namah Shivaya</Text>
       <Button title="Add Blog Post" onPress={addBlogPost} />
       <FlatList
-        data={data}
+        data={state}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({ item }) => <Text>{item.title}</Text>}
       />
